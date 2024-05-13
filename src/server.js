6c956c09c3a8cc9f50317a405c765f2dd6c5b765
reiyua <rei@reiyua.lol>
2024-05-13 18:18:26 +1000

    greek question mark moment
    just a little trolling buddo

--- src/server.js
+++ src/server.js
@@ -6,34 +6,34 @@ const app = fastify({ logger: false });
 const port = 80;
 
 app.listen({ host: "0.0.0.0", port }, () => {
-  console.log(`[Server] Fastify started listening on port ${port}`);
-});
+  console.log(`[Server] Fastify started listening on port ${port}`);
+});
 
 app.get("/", (req, res) => {
   res.type("text/html");
-  res.send(fs.readFileSync(path.join(process.cwd(), "public/index.html")));
-});
+  res.send(fs.readFileSync(path.join(process.cwd(), "public/index.html")));
+});
 
 app.get("/contact", (req, res) => {
-  res.type("text/html");
-  res.send(fs.readFileSync(path.join(process.cwd(), "public/contact.html")));
-});
+  res.type("text/html");
+  res.send(fs.readFileSync(path.join(process.cwd(), "public/contact.html")));
+});
 
 app.get("/assets/:type/:file", (req, res) => {
   const location = path.join(
     process.cwd(),
     `public/assets/${req.params.type}/${req.params.file}`
-  );
+  );
 
   if (fs.existsSync(location)) {
     return res
       .type(`text/${req.params.file.split(".")[1]}`)
-      .send(fs.readFileSync(location));
+      .send(fs.readFileSync(location));
   }
 
   res.status(404).send({
     message: `Route ${req.method}:${req.originalUrl} not found`,
     error: "Not Found",
     statusCode: 404,
-  });
-});
+  });
+});
